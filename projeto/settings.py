86cdbd35b644e20b8ed32dc4e70142e6f0bb26a2
Mauricio Gouveia <mauricioargj@gmail.com>
2021-04-29 08:57:26 -0300

    Quarto Commit

--- projeto/settings.py
+++ projeto/settings.py
@@ -27,7 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my-apps
     'projeto.core',
+    'projeto.produto',
 ]
 
 MIDDLEWARE = [
